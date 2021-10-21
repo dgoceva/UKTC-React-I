546c90fc824539a7f1e34df56aef0fb2b817ba16
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-21 11:45:10 +0300

    List component added - 21-10-21

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
+import "./App.css";
 
-const list=[
+const list = [
   {
-    title: 'React',
-    url: 'http://reactjs.org',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "http://reactjs.org",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'http://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "http://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
-]
+];
 
 function App() {
   return (
@@ -26,22 +26,28 @@ function App() {
 
       <label htmlFor="search">Search:</label>
       <input type="text" id="search" placeholder="Search" />
-      <hr/>
-      <ul>
-        {list.map(function(item){
-          return (
+      <hr />
+      <List />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
           <li key={item.objectID}>
             <span>
-              <a href={item.url}>{item.title}</a>
-              {" "}
+              <a href={item.url}>{item.title}</a>{" "}
             </span>
             <span>{item.author}&nbsp;</span>
-            <span>{item.num_comments+" "}</span>
+            <span>{item.num_comments + " "}</span>
             <span>{item.points}</span>
           </li>
-        )})}
-      </ul>
-    </div>
+        );
+      })}
+    </ul>
   );
 }
 
