3157e1c8653ca4767d7fede348996d406d7fe09b
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-28 12:07:36 +0300

    props added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,52 @@
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "http://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "http://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "http://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "http://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>My Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>{" "}
-        </span>
-        <span>{item.author}&nbsp;</span>
-        <span>{item.num_comments + " "}</span>
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>{" "}
+    </span>
+    <span>{props.item.author}&nbsp;</span>
+    <span>{props.item.num_comments + " "}</span>
+    <span>{props.item.points}</span>
+  </li>
 );
 
 const Search = () => {
@@ -61,11 +65,5 @@ const Search = () => {
     </div>
   );
 };
-
-const countPlusOne = (count) => {
-  return count + 1;
-};
-
-const countPlusOne1 = (count) => count + 1;
 
 export default App;
