1d8cfc8597860abda2741c9b9dccbe67079bab7c
dgoceva <dgoceva@abv.bg>
2021-12-12 08:59:23 +0200

    model name changed

--- employee_project/employee_register/models.py
+++ employee_project/employee_register/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Positon(models.Model):
+class Position(models.Model):
     title = models.CharField(max_length=50)
 
 
@@ -11,4 +11,4 @@ class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
     mobile = models.CharField(max_length=15)
-    position = models.ForeignKey(Positon, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
